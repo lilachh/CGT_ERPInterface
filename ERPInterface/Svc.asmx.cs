602aabd05a54b51542d8f01a4dfe1fd337105c08
謝文皞 <xiewenhao@gmail.com>
2017-06-30 22:50:49 +0800

    test

--- ERPInterface/Svc.asmx.cs
+++ ERPInterface/Svc.asmx.cs
@@ -12,11 +12,11 @@ namespace ERPInterface
     /// Summary description for Svc
     /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
-    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)] 
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
-    public class Svc : System.Web.Services.WebService
+    public class Svc : System.Web.Services.WebService   
     {
 
         [WebMethod]
